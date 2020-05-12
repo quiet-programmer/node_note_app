d919c7955e610db813bb0e11d75c0b4e86376c8c
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-13 00:31:30 +0100

    worked on add command

--- app.js
+++ app.js
@@ -1,6 +1,69 @@
+//node modules
+const chalk = require('chalk')
+const yargs = require('yargs')
 
+//personal required files
 const notes = require('./notes')
 
-const mynote = notes('Your Note....')
+//common functions
+const log = console.log
 
-console.log(mynote)
+//add, remove, read, list
+
+//Create Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new Note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        price: {
+            describe: 'Note Price',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        log(`Title: ${argv.title}`)
+        log(`Price: ${argv.price}`)
+        log(`Body: ${argv.body}`)
+    }
+})
+
+//Create Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove\'s a Note',
+    handler: function() {
+        log("Removing a Note...")
+    }
+})
+
+//Create Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read\'s a Note',
+    handler: function() {
+        log("Reading Note...")
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List\'s all the Note\'s',
+    handler: function() {
+        log('Listing all Note\'s...')
+    }
+})
+
+// log(yargs.argv)
+yargs.parse()
